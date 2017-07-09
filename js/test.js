c87dee7b7fbd0f1d9f3d014d7cd28430b2974185
Ignas Grigaravicius <kickeris@gmail.com>
2017-07-09 13:21:00 +0100

    Post card, js slider, subscription button css added

--- js/test.js
+++ js/test.js
@@ -22,3 +22,45 @@
              'show more <span class="glyphicon glyphicon-triangle-bottom"></span> ' : 'show less <span class="glyphicon glyphicon-triangle-top"></span>');
         });
 });
+  $(document).ready(function(){
+    $(".flipPic").click(function(){
+        $(".panelPic").slideToggle("slow");
+    });
+});
+
+  $(document).ready(function(){
+        $(".flipPic").click(function(){
+            $(this).html($(this).html() == 'show less <span class="glyphicon glyphicon-triangle-top"></span>' ?
+             'show more <span class="glyphicon glyphicon-triangle-bottom"></span> ' : 'show less <span class="glyphicon glyphicon-triangle-top"></span>');
+        });
+});
+// link scroller
+$(document).ready(function(){
+  $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').animate({
+        'scrollTop': $target.offset().top
+      }, 1000, 'swing')
+        window.location.hash = target;
+      });
+  });
+
+// to top scroller
+$(document).ready(function(){
+      $('body').append('<div id="toTop"</div>');
+    	$(window).scroll(function () {
+			if ($(this).scrollTop() != 0) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		}); 
+    $('#toTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
