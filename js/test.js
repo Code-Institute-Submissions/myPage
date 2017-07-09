a81ee7ed4f6589bd097233abaa6c020c6b07fce0
Ignas Grigaravicius <kickeris@gmail.com>
2017-07-09 11:17:53 +0100

    Gallery modal added, bg images fixed

--- js/test.js
+++ js/test.js
@@ -10,3 +10,15 @@
              'show my photograph gear <span class="glyphicon glyphicon-triangle-bottom"></span> ' : 'hide my photograph gear <span class="glyphicon glyphicon-triangle-top"></span>');
         });
 });
+  $(document).ready(function(){
+    $("#gallery").click(function(){
+        $("#galleryDrop").slideToggle("slow");
+    });
+});
+
+  $(document).ready(function(){
+        $("#gallery").click(function(){
+            $(this).html($(this).html() == 'show less <span class="glyphicon glyphicon-triangle-top"></span>' ?
+             'show more <span class="glyphicon glyphicon-triangle-bottom"></span> ' : 'show less <span class="glyphicon glyphicon-triangle-top"></span>');
+        });
+});
